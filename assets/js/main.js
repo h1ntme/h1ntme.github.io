c214d5d2a7edc10cba7a677258e1e5d4c54bdc86
DonPrus <donprusne@gmail.com>
2015-02-17 21:08:40 +0300

    sos :)

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ $( "#search" ).autocomplete({
 	}
 });
 
-navigator.vibrate([1000, 500, 1000]);
+window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
 
  $('.droppable').hover(
         function () {
